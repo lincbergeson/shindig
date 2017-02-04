643190be1460ed1d5bd9e432e5dba10fe0e6f0da
Lincoln Bergeson <lincoln.bergeson@gmail.com>
2017-02-04 14:59:17 -0700

    Removed sensitive information

--- db_init.php
+++ db_init.php
@@ -1,3 +1,3 @@
-<?php
+<?php 
 $dbh = new PDO('mysql:host=localhost;dbname=shindig', 'shindig', 'SeQMnpToMvvTY7Uf', array(PDO::ATTR_PERSISTENT => true));
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
